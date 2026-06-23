573c18b770076c3855b764955207d79bf7080d77
Wolf Intelegence <halakuwolfintelligencepk@gmail.com>
2026-06-23 10:12:08 +0500

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,11 @@
 <?php
-// logout.php
+// logout.php - Clear session and cookie
 session_start();
 session_destroy();
+
+// Clear cookie
+setcookie('user_session', '', time() - 3600, '/');
+
 header('Location: index.php');
 exit;
 ?>
